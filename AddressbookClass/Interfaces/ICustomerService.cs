f37ea59e66e903f4ce5d839cc3e7e1df92ea2209
kushtrim <kushtrim_shabooni99@hotmail.com>
2023-12-13 17:23:34 +0100

    Cosmetic changes in interfaces

--- AddressbookClass/Interfaces/ICustomerService.cs
+++ AddressbookClass/Interfaces/ICustomerService.cs
@@ -7,5 +7,5 @@ public interface ICustomerService
     bool AddCustomerTolist(ICustomer customer);
     IEnumerable<ICustomer> GetCustomersFromList();
     ICustomer GetCustomerFromList(string email);
-    public bool RemoveCustomer(string email);
+    bool RemoveCustomer(string email);
 }
